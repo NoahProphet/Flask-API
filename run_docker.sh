836aabbf46354e7c5c2b9b70d048334000d81346
root <root@kali>
2024-11-14 18:53:14 +0330

    adding kong to services

--- run_docker.sh
+++ run_docker.sh
@@ -2,6 +2,7 @@ echo killing old docker processes
 docker-compose down -v --remove-orphans
 docker volume ls | awk '{print $2}' | xargs -I % docker volume rm %
 sudo rm -r ./postgres-data
+sudo rm -r /postgres-kong
 
 echo delete database volume
 docker volume rm flask-on-docker_postgres_data
