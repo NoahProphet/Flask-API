9c764737ae396985c7e1dde90fccc4da660a2676
NoahProphet <alipour.sina@protonmail.com>
2024-12-07 18:29:53 +0330

    prometheus plugin

--- run_docker.sh
+++ run_docker.sh
@@ -2,9 +2,12 @@ echo killing old docker processes
 docker-compose down -v --remove-orphans
 docker volume ls | awk '{print $2}' | xargs -I % docker volume rm %
 sudo rm -r ./postgres-data
-sudo rm -r /postgres-kong
+sudo rm -r ./postgres-kong
+sudo rm -r ./prometheus-data
+sudo rm -r ./grafana-data
 
-echo delete database volume
+
+echo delete All volumes volume
 docker volume rm flask-on-docker_postgres_data
 
 echo building docker containers
